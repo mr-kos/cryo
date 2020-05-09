264f3cef0766ea4516bf86c4fe9bec898ecc7ee4
mr-kos <mr.ilyakos@gmail.com>
2020-05-09 12:47:22 +0000

    add custom assimilator

--- cryo_project/images/makeproject/create_jobs.sh
+++ cryo_project/images/makeproject/create_jobs.sh
@@ -5,9 +5,9 @@ i=1
 while [ $i -le $1 ]
 do
 
-echo "Job № "$i
+echo "Job "$i
 
-bin/boinc2docker_create_work.py --target_nresults 1 --target_user $i ozon67/cryo-client:test
+bin/boinc2docker_create_work.py --min_quorum 1 --target_nresults 1 --target_host $i ozon67/cryo-client:test
 
 let "i=i+1"
 
