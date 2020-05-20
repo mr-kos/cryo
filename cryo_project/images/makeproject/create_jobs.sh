ad40216b388c0f86d262a332b6ff18216b29d03b
mr-kos <mr.ilyakos@gmail.com>
2020-05-20 22:54:54 +0000

    fix

--- cryo_project/images/makeproject/create_jobs.sh
+++ cryo_project/images/makeproject/create_jobs.sh
@@ -7,7 +7,7 @@ do
 
 echo "Job "$i
 
-bin/boinc2docker_create_work.py --min_quorum 1 --target_nresults 1 --target_host $i ozon67/cryo-client:test
+bin/boinc2docker_create_work.py --min_quorum 2 --target_nresults 2 ozon67/cryo-client:test
 
 let "i=i+1"
 
